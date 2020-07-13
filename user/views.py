a8dcf4844d915b3ed6e6a15723ca3ad9a79363b4
Orinda Harrison <ajuongharry35@gmail.com>
2020-07-13 22:42:54 +0300

    Bug fix and dependencies update

--- user/views.py
+++ user/views.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import parsers
 from xauth.views.profile import ProfileView as XauthProfileView
 from xauth.views.signup import SignUpView as XauthSignUpView
@@ -12,6 +14,20 @@ class SignUpView(XauthSignUpView):
     parser_classes = (parsers.MultiPartParser, parsers.JSONParser,)
     serializer_class = SignUpRequestWithPhotoSerializer
     serializer_class_response = SignUpResponseSerializer
+
+    def get_request_data(self, request):
+        rd = request.data  # request data
+        # rd1: {"user":{ "user": {}, "password": "12345" },"photo": <File>}
+        photo, user, password = rd.get('photo'), rd.get('user'), rd.get('password')
+        # if rd1 user retrieval failed, fallback to { "user": {}, "password": "12345" }
+        user = rd if user is None else user
+        # if user data was received as string decode it to dict
+        user = json.loads(user) if isinstance(user, (str, bytes)) else user
+        password = password or user.get('password')
+        user = user.get('user', user)
+        user['password'] = password
+        user['photo'] = photo
+        return user
 
 
 class ProfileView(XauthProfileView):

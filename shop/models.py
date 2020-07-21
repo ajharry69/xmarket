6cbe35aab60b58586537f3f3f48f8e0571d024dd
Orinda Harrison <ajuongharry35@gmail.com>
2020-07-21 22:29:00 +0300

    Split shop LocationAddress model to PhysicalAddress and Coordinates models

--- shop/models.py
+++ shop/models.py
@@ -5,10 +5,6 @@ from django.db import models
 class Shop(models.Model):
     name = models.CharField(blank=False, null=False, max_length=150, )
     tax_pin = models.CharField(max_length=50, )
-
-
-class Contact(models.Model):
-    shop = models.OneToOneField(Shop, on_delete=models.CASCADE)
     phone = models.CharField(blank=True, null=True, max_length=20, )
     email = models.CharField(max_length=200, blank=True, null=True, )
 
@@ -19,12 +15,26 @@ class PostalAddress(models.Model):
     code = models.IntegerField(default=-1)
 
 
-class LocationAddress(models.Model):
+class Location(models.Model):
+    shop = models.OneToOneField(Shop, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class PhysicalAddress(Location):
     __DEFAULT_COUNTRY = pycountry.countries.get(alpha_2='KE').name
     __COUNTRIES = [(c.name, c.name) for c in list(pycountry.countries)]
-    shop = models.OneToOneField(Shop, on_delete=models.CASCADE)
-    town = models.CharField(blank=True, null=True, max_length=100, )
-    country = models.CharField(choices=__COUNTRIES, default=__DEFAULT_COUNTRY, blank=True, null=True, max_length=100, )
+    country = models.CharField(
+        choices=__COUNTRIES, default=__DEFAULT_COUNTRY,
+        blank=True, null=True, max_length=100,
+    )
+    major_town = models.CharField(blank=True, null=True, max_length=100, )
+    specific_town = models.CharField(blank=True, null=True, max_length=100, )
+    street = models.CharField(blank=True, null=True, max_length=250, )
+
+
+class Coordinates(Location):
     latitude = models.CharField(blank=True, null=True, max_length=50, )
     longitude = models.CharField(blank=True, null=True, max_length=50, )
     altitude = models.CharField(blank=True, null=True, max_length=50, )
--- shop/signals.py
+++ shop/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save
 
-from .models import LocationAddress
+from .models import Coordinates, PhysicalAddress
 
 
-def on_location_address_post_save(sender, instance, created, **kwargs):
+def on_physical_address_post_save(sender, instance, created, **kwargs):
     """
     retrieve and update geographical coordinates on created
     """
@@ -11,4 +11,13 @@ def on_location_address_post_save(sender, instance, created, **kwargs):
         pass
 
 
-post_save.connect(on_location_address_post_save, LocationAddress, dispatch_uid='location_address01')
+def on_coordinates_post_save(sender, instance, created, **kwargs):
+    """
+    retrieve and update geographical coordinates on created
+    """
+    if created:
+        pass
+
+
+post_save.connect(on_physical_address_post_save, PhysicalAddress, dispatch_uid='physical_address01')
+post_save.connect(on_coordinates_post_save, Coordinates, dispatch_uid='location_coordinates01')
